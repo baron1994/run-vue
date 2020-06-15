2f256da4c89653e5caf0f17fa72a06fd3a233dcb
baron1994 <1942992735@qq.com>
2020-06-15 23:40:47 +0800

    init empty project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Edit />
+    <Show />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Edit from "@/components/edit.vue";
+import Show from "@/components/show.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Edit,
+    Show
   }
-}
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+}
+#app {
+  display: flex;
+}
+#app > div {
+  width: 50%;
+  height: 100%;
 }
 </style>
---
+++ src/components/edit.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    edit
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
